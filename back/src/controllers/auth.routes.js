5786b7c0441845fb301e8a31fdf09727ad27b8f3
flaviolunaferreira <flaviolunaferreira@gmail.com>
2025-11-27 07:58:19 -0300

    adicionando rotas na autenticação e swagger

---
+++ back/src/controllers/auth.routes.js
@@ -0,0 +1,62 @@
+// src/routes/auth.routes.js
+
+const express = require('express');
+const authController = require('../controllers/auth.controller');
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ * name: Autenticação
+ * description: Cadastro e Login de Usuários
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ * post:
+ * summary: Cadastra um novo usuário
+ * tags: [Autenticação]
+ * requestBody:
+ * required: true
+ * content:
+ * application/json:
+ * schema:
+ * $ref: '#/components/schemas/UserRegister'
+ * responses:
+ * 201:
+ * description: Usuário criado com sucesso.
+ * 409:
+ * description: E-mail já cadastrado.
+ */
+router.post('/register', authController.registrar);
+
+/**
+ * @swagger
+ * /auth/login:
+ * post:
+ * summary: Autentica o usuário e retorna o token JWT
+ * tags: [Autenticação]
+ * requestBody:
+ * required: true
+ * content:
+ * application/json:
+ * schema:
+ * $ref: '#/components/schemas/UserLogin'
+ * responses:
+ * 200:
+ * description: Login bem-sucedido. Retorna o token de acesso.
+ * content:
+ * application/json:
+ * schema:
+ * type: object
+ * properties:
+ * token:
+ * type: string
+ * 401:
+ * description: Credenciais inválidas.
+ */
+router.post('/login', authController.login);
+
+module.exports = router;
